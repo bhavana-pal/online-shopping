a4342bfb27a5aceafbf52e3f887ed414352aa383
RAJNI <RAJNI@RAJNI-PC>
2017-02-18 00:45:38 +0530

    backend done

---
+++ online-shoppingBackEnd/src/main/java/net/bp/online_shoppingBackEnd/App.java
@@ -0,0 +1,13 @@
+package net.bp.online_shoppingBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
